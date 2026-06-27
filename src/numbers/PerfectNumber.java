81b982271bf8410d8e2995609a3bb1320b9fa000
SkNaserHussain <sknaserhussain@gmail.com>
2026-06-27 19:14:50 +0530

    Initial commit

---
+++ src/numbers/PerfectNumber.java
@@ -0,0 +1,16 @@
+package numbers;
+
+public class PerfectNumber {
+public static boolean isPerfect(int num) {
+	int sum=0;
+	for(int i=1;i<num;i++) {
+		if(num%i==0) {
+	        sum+=i;	
+	}
+}
+return sum==num;
+}
+public static void main(String[]args) {
+	System.out.println(isPerfect(15));
+}
+}
