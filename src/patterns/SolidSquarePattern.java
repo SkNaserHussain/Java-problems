84768f296190dd37f16bbae035c417a5a49602cd
SkNaserHussain <sknaserhussain@gmail.com>
2026-07-05 17:59:35 +0530

    Initial commit

---
+++ src/patterns/SolidSquarePattern.java
@@ -0,0 +1,19 @@
+/*
+ * Write a Java program to print a square pattern of * having n rows and n columns.
+ */
+
+package patterns;
+
+public class SolidSquarePattern {
+public static void squarePattern(int n) {	 
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+}
+public static void main(String[] args) {
+	squarePattern(4);
+}
+}
